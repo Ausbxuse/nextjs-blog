cfca0a6dfc5d1d8c5f90a00dae60400b17a88935
Peter <peterzhaozzy@gmail.com>
2021-11-08 19:49:33 -0800

    add proper hamburger menu and todo list

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head'
 // import Image from 'next/image'
 import styles from './nav.module.css'
@@ -5,83 +6,52 @@ import styles from './nav.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import { slide as Menu } from 'react-burger-menu'
+import Burger from '@animated-burgers/burger-squeeze' 
+// don't forget the styles
+import '@animated-burgers/burger-squeeze/dist/styles.css' 
 
-var burgerStyles = {
-    bmBurgerButton: {
-	position: 'fixed',
-	width: '1.5rem',
-	height: '1rem',
-	right: '1.5rem',
-	top: '1.2em'
-    },
-    bmBurgerBars: {
-	background: '#373a47'
-    },
-    bmBurgerBarsHover: {
-	background: '#a90000'
-    },
-    bmCrossButton: {
-	height: '3rem',
-	width: '3rem'
-    },
-    bmCross: {
-	background: '#bdc3c7'
-    },
-    bmMenuWrap: {
-	top: '3.2em',
-	position: 'fixed',
-	height: '100%'
-    },
-    bmMenu: {
-	background: '#ffffff',
-	padding: '2.5em 1.5em 0',
-	fontSize: '1.15em'
-    },
-    bmMorphShape: {
-	fill: '#373a47'
-    },
-    bmItemList: {
-	// color: '#b8b7ad',
-	padding: '0.8em'
-    },
-    bmItem: {
-	textAlign: 'center',
-	margin: '0.8em'
-    },
-    bmOverlay: {
-	background: 'rgba(0, 0, 0, 0)'
-    }
+var burgerOpen = false
+
+function handleClick() {
+    burgerOpen = !burgerOpen
 }
 
 export default function Nav({children}) {
+    const [showMobileNav, setShowMobileNav] = useState(false);
+    function toggle(){
+	setShowMobileNav(!showMobileNav);
+    }
+
     return (
 	<>
 	    <div className={styles.headerContainer}>
 		<div className={styles.topHint}></div>
 		<nav className={styles.navContainer}>
 		    <ul className={styles.navItems}>
-			<li><Link href="index">Home</Link></li>
+			<li><Link href="/">Home</Link></li>
 			<li><Link href="blog">Blog</Link></li>
 			<li><Link href="contact">Contact</Link></li>
 			<li><Link href="donate">Donate</Link></li>
 		    </ul>
 		    <div className={styles.mobileNav}>
+			<a> Home </a>
 			<Link href="/">
-			    <img
+			    <Image
 				src="/images/snappy-logo.png"
 				className={styles.logo}
+				width={44}
+				height={44}
 			    />
 			</Link>
-			<Menu right width={ '100%' } styles={ burgerStyles }>
-			    <a href="/">Home</a>
-			    <a href="/about">About</a>
-			    <a href="/contact">Contact</a>
-			    <a href="/blog">Blog</a>
-			    <a href="/donate">Donate</a>
-			</Menu>
+			<Burger onClick={toggle} isOpen={ showMobileNav } />
 		    </div>
 		</nav>
+		<div className={!showMobileNav ? styles.mobileNavList : styles.mobileNavListActive}>
+				     <li><Link href="/">Home</Link></li>
+				     <li><Link href="blog">Blog</Link></li>
+				     <li><Link href="contact">Contact</Link></li>
+				     <li><Link href="donate">Donate</Link></li>
+				 </div>
 	    </div>
 
 	    <main>{children}</main>
